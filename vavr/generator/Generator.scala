6692d705d502b0d4dacdf9d2f66bd52df0187ffa
Boris van Katwijk <bvkatwijk@gmail.com>
2025-05-10 13:54:33 +0200

    start curry tests

--- vavr/generator/Generator.scala
+++ vavr/generator/Generator.scala
@@ -3486,6 +3486,28 @@ def generateTestClasses(): Unit = {
 
               }
 
+              @Nested
+              class CurryTests {
+                ${(1 to i).gen(j =>
+                  val genArgs = (1 to i).gen(k => "String")(", ")
+                  val params = (1 to i).gen(k => s"String s$k")(", ")
+                  val values = (1 to i).gen(k => if (k == j) "\"xx\"" else s"\"s$k\"")(", ")
+                  val expected = (1 to i).gen(k => if (k == j) "XX" else s"s$k")("")
+                  val concat = (1 to i).gen(k => s"s$k")(" + ")
+                  xs"""
+
+                  @$test
+                  public void shouldCompose$j() ${checked.gen(" throws Throwable ")}{
+                      final $name$i<$genArgs, String> concat = ($params) -> $concat;
+                      final Function1<String, String> toUpperCase = String::toUpperCase;
+                      assertThat(concat.compose$j(toUpperCase).apply($values)).isEqualTo(\"$expected\");
+                  }
+
+                  """
+                )}
+
+              }
+
               ${(i == 0).gen(xs"""
               @$test
               public void shouldNarrow()${checked.gen(" throws Throwable")}{
